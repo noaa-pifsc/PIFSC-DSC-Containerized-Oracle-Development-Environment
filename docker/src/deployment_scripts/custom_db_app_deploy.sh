4a8450daa92b0d1ca479880aab1896056798131f
Jesse Abdul <jesse.abdul@noaa.gov>
2025-11-25 07:23:45 -1000

    Bug fix to remove redundant source ./config/custom_container_config.sh call since it's already been moved to db_app_deploy.sh since the current directory may not be the same after the db_app_deploy.sh code runs (cd /tmp/apex)

--- docker/src/deployment_scripts/custom_db_app_deploy.sh
+++ docker/src/deployment_scripts/custom_db_app_deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# load the bash configuration file
-source ./config/custom_container_config.sh
-
 echo "running the custom database and/or application deployment scripts"
 
 # run each of the sqlplus scripts to deploy the schemas, objects for each schema, applications, etc.
