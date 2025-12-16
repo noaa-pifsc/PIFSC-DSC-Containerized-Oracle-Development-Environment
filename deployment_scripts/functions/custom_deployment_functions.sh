16398367b0a42e0ea07ce479d606071fefc96c57
Jesse Abdul <jesse.abdul@noaa.gov>
2025-12-16 13:34:25 -1000

    Simplifying the docker deployment script (removed preparation scripts and implemented CDS submodule)
    modularized the docker-compose.yml files so ords and the DB named volume can be easily omitted
    
    Implemented a variable check on TARGET_APEX_VERSION to skip apex installation/upgrade if the variable is empty

---
+++ deployment_scripts/functions/custom_deployment_functions.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# function that deploys the containers for dev/test/prod environments
+build_deploy_dev_environment ()
+{
+	# build the list of compose files:
+	local COMPOSE_FILES=("--env-file" "./docker/.env")
+	COMPOSE_FILES+=("-f" "docker/CODE-db-deploy.yml")
+
+	if [ "$ENV_NAME" == "dev" ]; then
+		# this is intended for the development environment (retain database and ords volumes)
+		COMPOSE_FILES+=("-f" "docker/CODE-db-named-volumes.yml")
+	fi
+
+	COMPOSE_FILES+=("-f" "docker/custom-docker-compose.yml")
+
+	# build and execute the docker container for the specified deployment environment:
+	docker compose "${COMPOSE_FILES[@]}" up -d --build
+}
